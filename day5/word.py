012d1d883fef3913988a7705eff6fb7c1bb2ba30
shamlik <tshamlikt@gmail.com>
2015-10-31 21:15:24 +0530

    word_game initial commit

--- day5/word.py
+++ day5/word.py
@@ -11,9 +11,9 @@ def wordlist():
     return clean_words
 
 
+
 def select(wordlist):
     return random.sample(wordlist,1)[0]
-
 
 
 
@@ -21,32 +21,35 @@ def game(word):
 
     error=0
     le = len(word)
-    print " ua word is {}".format("*"*len(word))
+    print " ua word is {}  10 Lives left".format("*"*len(word))
     print word
     list2=[]
+    
     for x in word:
-        #list1.append(x)
         list2.append("*")
         
-    for x in range(10):
-        
+    while error < 10:
         alpha=raw_input()
+        
         if alpha in word:
             ind = [x for x in range(len(word)) if alpha == word[x]]
             for x in ind:
                 list2[x]=alpha
-            print ''.join(list2)
         else:
             error=error+1
-            print "{}   {}".format(error,''.join(list2))
+        
+        print "ua  word = {}  {} Lives Left".format(''.join(list2),10-error)
+        
+        if error == 10 :
+            print " {} Game Over {}".format(10*"%",10*"%")
+        elif not("*" in list2):
+             print " {} GOTCHA {}".format(10*"%",10*"%")
+             error=11
 
+        
 
+        
             
-    
-
-
-                
-    
     
 
 
