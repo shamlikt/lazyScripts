560e956a93f94666158f2f62ef1778f0335174b5
shamlik <tshamlikt@gmail.com>
2015-11-02 21:13:49 +0530

    use while loop , replace % operator

--- sieves_algorithm.py
+++ sieves_algorithm.py
@@ -1,12 +1,13 @@
 def prime(limit):
     list2=range(2,limit)
     prime=[]
-    for x in list2:
-        list2=[y for y in list2 if  y%x]
-        if len(list2) !=0:
-            prime.append(list2.pop(0))
+    while len(list2):
+        primeD=(list2.pop(0))
+        prime.append(primeD)
+        list2=[x for x in list2 if x-(x/primeD)*primeD]
     return prime
- 
+        
+
 
 
 
