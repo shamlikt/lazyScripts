23ff599379914cbd67f9e67e12a028401505b767
shamlik <tshamlikt@gmail.com>
2015-11-03 00:48:42 +0530

    use Key Value to remove

--- sieves_algorithm.py
+++ sieves_algorithm.py
@@ -1,17 +1,21 @@
+
 import sys
-
+ 
 def prime(limit):
-    list2=range(3,limit,2)
-    prime=[];prime.append(2)
-    while len(list2):
-        primeD=(list2.pop(0))
-        prime.append(primeD)
-        list2=[x for x in list2 if x-(x/primeD)*primeD]
+    limit=limit+1
+    noprime=[False]*limit
+    prime=[]
+    for i in range(2,limit):
+        if noprime[i]:
+            continue
+        else:
+            for y in range(i*2,limit,i):
+                noprime[y]=[True]
+            prime.append(i)
     return prime
-        
-
-
-       
+                
+            
+            
 
 if __name__ == '__main__':
     print prime(int(sys.argv[1]))
