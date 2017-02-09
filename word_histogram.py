07573a584dc716640a8c476d4de2ca7e4f7bb0d3
shamlik <tshamlikt@gmail.com>
2017-02-09 11:47:16 +0530

    Print alphabetic cont in stars

---
+++ word_histogram.py
@@ -0,0 +1,7 @@
+''' This script print alpahbetic histogram of give word
+'''
+import collections
+dic = collections.Counter(raw_input('Enter string: '))
+for i in range(max(dic.values()),-1, -1):
+    print ' '.join(['*'  if i < j else ' ' for j in dic.values()])
+print ' '.join(dic.keys())
